6e9b901af4b2dc1286e03a6d95f291790a60d44d
Kutay Koralturk <koralturkk@gmail.com>
2021-03-12 03:23:06 +0300

    Delete config.py

--- app/settings/config.py
+++
@@ -1,5 +0,0 @@
-PROJECT_NAME = "ML Deployment"
-DB_NAME = "MLDeployment"
-DB_PASSWORD = "{t89kZpIDU#,l%{9y8^p"
-DB_USERNAME = "koralturkk"
-DB_URL = "mongodb+srv://koralturkk:{}@cluster0.eytit.mongodb.net/{}?retryWrites=true&w=majority".format(DB_NAME, DB_PASSWORD)
